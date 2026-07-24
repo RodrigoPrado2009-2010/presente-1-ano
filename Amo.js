c7eef07423b2b2124f1a3a7762e79c539a106f11
RodrigoPrado2009-2010 <rodrigoprudencio2009@gmail.com>
2026-07-24 19:15:33 -0300

    Fix typo in image path for opcaoAbraco

--- Amo.js
+++ Amo.js
@@ -505,7 +505,7 @@ const previewTrocaPersonagens = document.getElementById(
 
 const gifsOriginais = {
   opcaoBeijo: "Imagens/beiju.gif",
-  opcaoAbraco: "Imagens/abraço.gif",
+  opcaoAbraco: "Imagens/abraco.gif",
   opcaoDormindo: "Imagens/dormindo.gif",
 };
 
